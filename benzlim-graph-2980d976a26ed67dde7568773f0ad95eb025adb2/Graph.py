539b5a0de1a0c81e4dbcc3b53971b21873934dd9
Gerald <gerald-wiese@web.de>
2017-11-23 16:32:46 +0100

    exceptions for reading files and logics for graph

---
+++ benzlim-graph-2980d976a26ed67dde7568773f0ad95eb025adb2/Graph.py
@@ -0,0 +1,66 @@
+class Graph:
+    EPS = 1
+    start = None
+    goal = None
+    nodes = []
+    breakpoints = []
+    capacity = 0
+    max_km = 0
+    gasInfo = {}
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current_gas = 0
+
+    def gas_for_km(self, km):
+        return km * 0.056
+        #todo: Should be car-specific
+
+    def km_for_gas(self, gas):
+        return gas / 0.056
+
+    def find_prevs(self):
+        for n in self.nodes:
+            cheapest = n
+            for i in self.nodes[0:self.nodes.index(n)]:
+                if i.distance_to(n) < self.capacity and i < cheapest:
+                    cheapest = i
+            n.prev = cheapest
+            if n.prev == n:
+                self.breakpoints.append(n)
+                 
+    def find_nexts(self):
+        for n in self.nodes[0:len(self.nodes)-1]:
+            cheapest = self.nodes[self.nodes.index(n)+1]
+            for i in self.nodes[self.nodes.index(n)+2:len(self.nodes)]:
+                #print("new cheapest?")
+                if n.distance_to(i) < self.capacity and i < cheapest:
+                    cheapest = i
+                    print("new cheapest!")
+            n.next = cheapest
+            
+    def drive_to_next(self):
+        x = self.start
+        bp = self.breakpoints
+        gas_info = {}
+
+        while x.distance_to(self.goal) > self.EPS:
+            if bp and x.distance_to(bp[0]) > 0 and self.gas_for_km(x.distance_to(bp[0])) < self.capacity:
+                gas_info[x.id] = self.current_gas - self.gas_for_km(x.distance_to(bp[0]))
+                self.current_gas += self.gas_for_km(x.distance_to(bp[0])) - self.gas_for_km(x.distance_to(bp[0]))
+                x = self.breakpoints.pop(0)
+            else:
+                self.current_gas -= self.gas_for_km(x.distance_to(x.next))
+                gas_info[x.id] = self.capacity - self.current_gas
+                self.current_gas = self.capacity
+                x = x.next
+        print(gas_info)
+
+
+
+'''
+DRIVE-TO-NEXT(i, k)
+1 Let x be i.
+2 If d xk ≤ U then just fill enough gas to go k.
+3 Otherwise, fill up and drive to next(x). Let x be next(x), go to step 2.
+'''
---
+++ benzlim-graph-2980d976a26ed67dde7568773f0ad95eb025adb2/InputFileReader.py
@@ -0,0 +1,63 @@
+import csv
+import datetime
+from Node import Node
+
+FILE = "Tankstellen.csv"
+
+
+def readRoute(pathRoute):
+    routeReader = csv.reader(open(pathRoute))
+    d = {}
+    try:
+        capacity=next(routeReader)
+        capacity=int(''.join(map(str,capacity)))
+        d["capacity"]=capacity
+    except ValueError:
+        print("ValueError")
+        d["capacity"]=-1
+    for row in routeReader:
+        date, id=row[0].split(";")
+        date_time, tz=date.split("+")
+        #timezone verarbeiten?
+        # dayte,timehh=date.split()
+        # year,month,day=dayte.split("-")
+        # time, hh=timehh.split("+")
+        # hour,minute,second=time.split(":")
+        #print(date)
+        try:
+            datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+            d[id] = date_time
+        except ValueError:
+            print("Incorrect data format at id %s, should be YYYY-MM-DD HH:MM:SS" %id)
+    return d
+
+
+def read_id(id):
+    headers = ['id', 'name', 'brand', 'adr', 'no', 'plz', 'ort', 'lat', 'lon']
+    with open(FILE) as cvsfile:
+        reader = csv.DictReader(cvsfile, fieldnames=headers, delimiter=';')
+        for row in reader:
+            if row['id'] == id:
+                lat = float(row['lat'])
+                lon = float(row['lon'])
+                return Node(id, lat, lon)
+        print('id %s not found' %id)
+        return None
+
+
+if __name__ == '__main__':
+    readRoute("bb.csv")
+    idList=[1,2,3,4,5,6,7,8,9] #??
+    #TODO:distance berechnet falsche werte?
+    #TODO:bei auslese-funktionen müssen unguenstige faelle abgefangen werden
+
+def readID(gasStationID, pathIDs):
+    idReader= csv.reader(open(pathIDs))
+    for row in idReader:
+        #TODO:try catch except irgendwas
+        currentID= [list(map(int, x)) for x in row[0]]
+        if currentID[0][0]==gasStationID:
+            lat=float(row[7])
+            lon=float(row[8])
+            return lat,lon
+    return -1,-1
